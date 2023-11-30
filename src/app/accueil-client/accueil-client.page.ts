2d6c67bdbbb2da74abd0083920adda952d04c212
Awa Doumbia <awa80598@gmail.com>
2023-11-30 15:01:06 +0000

    commande

--- src/app/accueil-client/accueil-client.page.ts
+++ src/app/accueil-client/accueil-client.page.ts
@@ -1,22 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { VenteService } from '../service/vente.service';
 
 @Component({
   selector: 'app-accueil-client',
   templateUrl: './accueil-client.page.html',
   styleUrls: ['./accueil-client.page.scss'],
 })
-export class AccueilClientPage implements OnInit {
-  
-  slideOpts = {
-    effect: 'cube', // Choisissez l'effet que vous voulez
-    speed: 1000, // Définissez la vitesse de transition en millisecondes
-    loop: true, // Activez la boucle des diapositives
-    autoplay: true, // Activez la lecture automatique des diapositives
-  };
+export class AccueilClientPage implements OnInit, AfterViewInit {
+  ventes: any[] = [];
+  constructor(private venteService: VenteService) {}
 
-  constructor() { }
+  translateValue = 0;
+  slideWidth = 0;
+  currentIndex = 0;
 
   ngOnInit() {
+    // Défilement automatique toutes les 2 secondes (ajustez selon vos besoins)
+    setInterval(() => this.nextSlide(), 2000);
+    this.getVente();
+  
   }
 
+  getVente(): void {
+    this.venteService.getVente().subscribe((response) => {
+      this.ventes = response.ventes;
+    });
+  }
+
+
+  ngAfterViewInit() {
+    this.slideWidth = document.querySelector('.image')?.clientWidth || 0;
+  }
+
+  nextSlide() {
+    const imageSlider = document.getElementById('image-slider') as HTMLDivElement;
+
+    if (this.currentIndex < imageSlider.children.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+
+    this.translateValue = -this.slideWidth * this.currentIndex;
+    imageSlider.style.transition = 'transform 0.5s ease-in-out';
+    imageSlider.style.transform = `translateX(${this.translateValue}px)`;
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -62,6 +62,10 @@ const routes: Routes = [
     path: 'contact',
     loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
   },
+  {
+    path: 'commande',
+    loadChildren: () => import('./commande/commande.module').then( m => m.CommandePageModule)
+  },
   
   
 
